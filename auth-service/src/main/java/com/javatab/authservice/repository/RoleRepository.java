126db5bf0c44a4602ee5c5bc24f4f92497b4051b
Nasruddin <nasruddin.java@gmail.com>
2022-03-02 10:48:04 +0530

    Update RoleRepository.java

--- auth-service/src/main/java/com/javatab/authservice/repository/RoleRepository.java
+++ auth-service/src/main/java/com/javatab/authservice/repository/RoleRepository.java
@@ -3,5 +3,6 @@ package com.javatab.authservice.repository;
 import com.javatab.authservice.domain.Role;
 import org.springframework.data.repository.CrudRepository;
 
+@Repository
 public interface RoleRepository extends CrudRepository<Role, String> {
 }
