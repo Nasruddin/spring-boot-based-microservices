38ecda9637d6e259ac13920250a94c190f6599a1
Nasruddin <nasruddin.java@gmail.com>
2022-04-18 16:07:19 +0530

    Edge service integrated using Spring Cloud Gateway

--- microservices/search-service/src/main/java/io/javatab/microservices/core/search/SearchServiceApplication.java
+++ microservices/search-service/src/main/java/io/javatab/microservices/core/search/SearchServiceApplication.java
@@ -9,22 +9,9 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("io.javatab")
-public class SearchServiceApplication implements CommandLineRunner {
-
-	private ElasticRepository repository;
-
-	public SearchServiceApplication(ElasticRepository repository) {
-		this.repository = repository;
-	}
+public class SearchServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchServiceApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		repository.deleteCourse().block();
-		repository.createCourse(new SearchEntity("Course Name", 1))
-				.subscribe(indexResponse -> System.out.println("Index Response " + indexResponse));
 	}
 }
