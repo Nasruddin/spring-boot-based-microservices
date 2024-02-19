8600bfd877ecbc3435cc514850eb97e67ab70339
Nasruddin <nasruddin.java@gmail.com>
2024-02-19 14:23:01 +0530

    Fixing bug in Authorization server

--- spring-cloud/authorization-server/src/main/java/io/javatab/springcloud/auth/jose/KeyGeneratorUtils.java
+++ spring-cloud/authorization-server/src/main/java/io/javatab/springcloud/auth/jose/KeyGeneratorUtils.java
@@ -14,10 +14,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+//CHECKSTYLE:OFF
+/*
+ * Copyright 2020-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.javatab.springcloud.auth.jose;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -25,12 +40,14 @@ import java.security.spec.ECFieldFp;
 import java.security.spec.ECParameterSpec;
 import java.security.spec.ECPoint;
 import java.security.spec.EllipticCurve;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 /**
  * @author Joe Grandja
  * @since 0.1.0
  */
-final class KeyGeneratorUtils {
+public final class KeyGeneratorUtils {
 
   private KeyGeneratorUtils() {
   }
@@ -59,18 +76,18 @@ final class KeyGeneratorUtils {
 
   static KeyPair generateEcKey() {
     EllipticCurve ellipticCurve = new EllipticCurve(
-        new ECFieldFp(
-            new BigInteger("115792089210356248762697446949407573530086143415290314195533631308867097853951")),
-        new BigInteger("115792089210356248762697446949407573530086143415290314195533631308867097853948"),
-        new BigInteger("41058363725152142129326129780047268409114441015993725554835256314039467401291"));
+            new ECFieldFp(
+                    new BigInteger("115792089210356248762697446949407573530086143415290314195533631308867097853951")),
+            new BigInteger("115792089210356248762697446949407573530086143415290314195533631308867097853948"),
+            new BigInteger("41058363725152142129326129780047268409114441015993725554835256314039467401291"));
     ECPoint ecPoint = new ECPoint(
-        new BigInteger("48439561293906451759052585252797914202762949526041747995844080717082404635286"),
-        new BigInteger("36134250956749795798585127919587881956611106672985015071877198253568414405109"));
+            new BigInteger("48439561293906451759052585252797914202762949526041747995844080717082404635286"),
+            new BigInteger("36134250956749795798585127919587881956611106672985015071877198253568414405109"));
     ECParameterSpec ecParameterSpec = new ECParameterSpec(
-        ellipticCurve,
-        ecPoint,
-        new BigInteger("115792089210356248762697446949407573529996955224135760342422259061068512044369"),
-        1);
+            ellipticCurve,
+            ecPoint,
+            new BigInteger("115792089210356248762697446949407573529996955224135760342422259061068512044369"),
+            1);
 
     KeyPair keyPair;
     try {
@@ -83,3 +100,4 @@ final class KeyGeneratorUtils {
     return keyPair;
   }
 }
+//CHECKSTYLE:ON
